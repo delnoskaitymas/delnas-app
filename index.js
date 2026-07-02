ff41e2e8b981b1e8be3be89551956addd3506281
delnoskaitymas <jurginajurginastr@gmail.com>
2026-07-02 20:34:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -507,9 +507,9 @@ app.post('/create-payment', async (req, res) => {
       currency: 'eur',
       metadata: { name: name || '', email: email || '' },
       ...(email ? {receipt_email: email} : {}),
-      automatic_payment_methods: { enabled: true },
-      payment_method_options: {
-        link: { display_preference: { preference: 'none' } }
+      automatic_payment_methods: {
+        enabled: true,
+        allow_redirects: 'never'
       }
     });
     res.json({ clientSecret: paymentIntent.client_secret, paymentIntentId: paymentIntent.id, sessionId: paymentIntent.id });
